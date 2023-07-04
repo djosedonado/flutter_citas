11183a8ee9a8f94d318330aa207197c3bf9bdf11
djosedonado <djosedonado@unicesar.edu.co>
2023-07-04 15:16:22 -0500

    Validacion de campos

--- lib/app/ui/pages/crear_cuenta.dart
+++ lib/app/ui/pages/crear_cuenta.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -8,8 +6,7 @@ import '../../controllers/usuarioController.dart';
 import '../../models/usuario.dart';
 import 'login.dart';
 
-class CrearCuentaPage extends StatefulWidget {  
-
+class CrearCuentaPage extends StatefulWidget {
   final Usuario? usuario;
 
   const CrearCuentaPage({super.key, this.usuario});
@@ -19,7 +16,6 @@ class CrearCuentaPage extends StatefulWidget {
 }
 
 class _CrearCuentaPageState extends State<CrearCuentaPage> {
-
   LoginController loginController = Get.find();
   UsuarioController usuarioController = Get.find();
 
@@ -31,8 +27,14 @@ class _CrearCuentaPageState extends State<CrearCuentaPage> {
   TextEditingController correoController = TextEditingController();
   TextEditingController contrasController = TextEditingController();
   TextEditingController confirmContrasController = TextEditingController();
-  
-  List<String> list = <String>['Seleccione','Masculino', 'Femenino', 'No Binario'];
+
+  List<String> list = <String>[
+    'Seleccione',
+    'Masculino',
+    'Femenino',
+    'No Binario'
+  ];
+  final _keyFrom = GlobalKey<FormState>();
 
   String dropdownValue = 'Seleccione';
   bool esEditar = false;
@@ -40,7 +42,7 @@ class _CrearCuentaPageState extends State<CrearCuentaPage> {
 
   @override
   void initState() {
-    if(widget.usuario != null){
+    if (widget.usuario != null) {
       Usuario usuario = widget.usuario!;
       esEditar = true;
       cedulaController.text = usuario.cedula!;
@@ -51,7 +53,7 @@ class _CrearCuentaPageState extends State<CrearCuentaPage> {
       correoController.text = usuario.correo!;
       dropdownValue = usuario.genero!;
     }
-    
+
     super.initState();
   }
 
@@ -59,12 +61,9 @@ class _CrearCuentaPageState extends State<CrearCuentaPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(esEditar ?'Editar Cuenta': 'Crea tu cuenta'),
+        title: Text(esEditar ? 'Editar Cuenta' : 'Crea tu cuenta'),
         titleTextStyle: const TextStyle(
-          color: Colors.blue,
-          fontSize: 30,
-          fontWeight: FontWeight.bold
-        ),
+            color: Colors.blue, fontSize: 30, fontWeight: FontWeight.bold),
         centerTitle: true,
         backgroundColor: Colors.white,
         elevation: 0,
@@ -78,150 +77,220 @@ class _CrearCuentaPageState extends State<CrearCuentaPage> {
             ),
           )
         ],
-        iconTheme: const IconThemeData(
-          color: Colors.black
-        ),
+        iconTheme: const IconThemeData(color: Colors.black),
       ),
-      body: ListView(
-        children: [
-          const SizedBox(height: 10,),
-          InputLogin(texto: 'Cedula',controller: cedulaController,tipoTeclado: TextInputType.number,),
-          const SizedBox(height: 10,),
-          InputLogin(texto: 'Nombres',controller: nombreController,),
-          const SizedBox(height: 10,),
-          InputLogin(texto: 'Apellidos',controller: apellidoController,),
-          const SizedBox(height: 10,),
-          InputLogin(texto: 'Telefono',controller: telefonoController,tipoTeclado: TextInputType.number,),
-          const SizedBox(height: 10,),
-          InputLogin(texto: 'Direccion',controller: direccionController,),
-          const SizedBox(height: 10,),
-          InputLogin(texto: 'Correo', controller: correoController, editable: !esEditar,),
-          const SizedBox(height: 10,),
-          esEditar ? Container() : Column(
-            children: [
-              InputLogin(texto: 'Contraseña', controller: contrasController, esContrasena: true,),
-              const SizedBox(height: 10,),
-              InputLogin(texto: 'Confirmacion de Contraseña',controller: confirmContrasController, esContrasena: true,),
-            ],
-          ),
-          const SizedBox(height: 20,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text('Genero')
-            ],
-          ),
-          Container(
-            margin: EdgeInsets.all(20),
-            padding: EdgeInsets.symmetric(horizontal:10),
-            decoration: BoxDecoration(
-              border: Border.all(width: 3, color: Colors.green),
-              borderRadius: BorderRadius.circular(10)
+      body: Form(
+        key: _keyFrom,
+        child: ListView(
+          children: [
+            const SizedBox(
+              height: 10,
             ),
-            child: DropdownButton(
-              value: dropdownValue,
-              icon: const Icon(Icons.arrow_downward),
-              elevation: 16,
-              style: const TextStyle(color: Colors.black),
-              underline: Container(
-                height: 0,
-              ),
-              onChanged: (String? value) {
-                setState(() {
-                  dropdownValue = value!;
-                });
-              },
-              items: list.map((e) => DropdownMenuItem(value: e, child: Text(e),)).toList()
-              ),
-          ),
-
-          esEditar && loginController.usuario.value.tipoUsuario == 3 ? Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Row(
-              children: [
-                Checkbox(
-                  checkColor: Colors.white,
-                  value: estado,
-                  onChanged: (bool? value) {                    
+            InputLogin(
+              texto: 'Cedula',
+              controller: cedulaController,
+              tipoTeclado: TextInputType.number,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            InputLogin(
+              texto: 'Nombres',
+              controller: nombreController,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            InputLogin(
+              texto: 'Apellidos',
+              controller: apellidoController,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            InputLogin(
+              texto: 'Telefono',
+              controller: telefonoController,
+              tipoTeclado: TextInputType.number,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            InputLogin(
+              texto: 'Direccion',
+              controller: direccionController,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            InputLogin(
+              texto: 'Correo',
+              controller: correoController,
+              editable: !esEditar,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            esEditar
+                ? Container()
+                : Column(
+                    children: [
+                      InputLogin(
+                        texto: 'Contraseña',
+                        controller: contrasController,
+                        esContrasena: true,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      InputLogin(
+                        texto: 'Confirmacion de Contraseña',
+                        controller: confirmContrasController,
+                        esContrasena: true,
+                      ),
+                    ],
+                  ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [Text('Genero')],
+            ),
+            Container(
+              margin: EdgeInsets.all(20),
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                  border: Border.all(width: 3, color: Colors.green),
+                  borderRadius: BorderRadius.circular(10)),
+              child: DropdownButton(
+                  value: dropdownValue,
+                  icon: const Icon(Icons.arrow_downward),
+                  elevation: 16,
+                  style: const TextStyle(color: Colors.black),
+                  underline: Container(
+                    height: 0,
+                  ),
+                  onChanged: (String? value) {
                     setState(() {
-                      estado = value!;
+                      dropdownValue = value!;
                     });
                   },
-                ),
-                const Text('Activar paciente'),
+                  items: list
+                      .map((e) => DropdownMenuItem(
+                            value: e,
+                            child: Text(e),
+                          ))
+                      .toList()),
+            ),
+            esEditar && loginController.usuario.value.tipoUsuario == 3
+                ? Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: [
+                        Checkbox(
+                          checkColor: Colors.white,
+                          value: estado,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              estado = value!;
+                            });
+                          },
+                        ),
+                        const Text('Activar paciente'),
+                      ],
+                    ),
+                  )
+                : Container(),
+            Column(
+              children: [
+                ElevatedButton(
+                    onPressed: () async {
+                      if (_keyFrom.currentState!.validate()) {
+                        if (!cedulaController.text.isNum) {
+                          Get.snackbar('Error', 'La cedula es numerica',
+                              snackPosition: SnackPosition.BOTTOM);
+                          return;
+                        }
+                        if (!telefonoController.text.isNum) {
+                          Get.snackbar('Error', 'La cedula es numerica',
+                              snackPosition: SnackPosition.BOTTOM);
+                          return;
+                        }
+                        if (cedulaController.text.isEmpty ||
+                            nombreController.text.isEmpty ||
+                            apellidoController.text.isEmpty ||
+                            telefonoController.text.isEmpty ||
+                            direccionController.text.isEmpty ||
+                            correoController.text.isEmpty) {
+                          Get.snackbar(
+                              'Error', 'Todos los datos son obligatorios',
+                              snackPosition: SnackPosition.BOTTOM);
+                          return;
+                        }
+                        if (!esEditar) {
+                          if (contrasController.text.isEmpty ||
+                              confirmContrasController.text.isEmpty) {
+                            Get.snackbar(
+                                'Error', 'Todos los datos son obligatorios',
+                                snackPosition: SnackPosition.BOTTOM);
+                            return;
+                          }
+                          if (contrasController.text !=
+                              confirmContrasController.text) {
+                            Get.snackbar(
+                                'Error', 'Las contraseñas no coinciden',
+                                snackPosition: SnackPosition.BOTTOM);
+                            return;
+                          }
+                        }
+
+                        Usuario tempUsuario = Usuario(
+                            correo: correoController.text,
+                            contras: contrasController.text,
+                            tipoUsuario: 1,
+                            nombres: nombreController.text,
+                            apellido: apellidoController.text,
+                            cedula: cedulaController.text,
+                            direccion: direccionController.text,
+                            genero: dropdownValue,
+                            telefono: telefonoController.text,
+                            estado: estado);
+                        if (esEditar) {
+                          tempUsuario.id = widget.usuario!.id!;
+                          bool response = await usuarioController
+                              .actualizarUsuario(tempUsuario);
+                          if (response) {
+                            Get.snackbar(
+                                'Actualizado', 'Se actualizó correctamente',
+                                snackPosition: SnackPosition.BOTTOM);
+                          } else {
+                            Get.snackbar('Error',
+                                'Ha ocurrido un error al actualizar la cuenta',
+                                snackPosition: SnackPosition.BOTTOM);
+                          }
+                        } else {
+                          Usuario? usuRes =
+                              await loginController.registraEmail(tempUsuario);
+                          if (usuRes != null) {
+                            Get.snackbar('Creado', 'Se creo correctamente',
+                                snackPosition: SnackPosition.BOTTOM);
+                          } else {
+                            Get.snackbar('Error',
+                                'Ha ocurrido un error al crear la cuenta',
+                                snackPosition: SnackPosition.BOTTOM);
+                          }
+                        }
+                      }
+                    },
+                    child: Text(esEditar ? 'Modificar' : 'Registrarse')),
               ],
             ),
-          ) : Container(),
-          Column(
-            children: [
-              ElevatedButton(
-                onPressed: () async {
-
-                  if(!cedulaController.text.isNum){
-                    Get.snackbar('Error', 'La cedula es numerica', snackPosition: SnackPosition.BOTTOM);
-                    return;
-                  }
-                  if(!telefonoController.text.isNum){
-                    Get.snackbar('Error', 'La cedula es numerica', snackPosition: SnackPosition.BOTTOM);
-                    return;
-                  }
-                  if(
-                    cedulaController.text.isEmpty ||
-                    nombreController.text.isEmpty ||
-                    apellidoController.text.isEmpty ||
-                    telefonoController.text.isEmpty ||
-                    direccionController.text.isEmpty ||
-                    correoController.text.isEmpty
-                  ){
-                    Get.snackbar('Error', 'Todos los datos son obligatorios', snackPosition: SnackPosition.BOTTOM);
-                    return;
-                  }
-                  if(!esEditar){
-                    if(contrasController.text.isEmpty || confirmContrasController.text.isEmpty){
-                      Get.snackbar('Error', 'Todos los datos son obligatorios', snackPosition: SnackPosition.BOTTOM);
-                      return;
-                    }
-                    if(contrasController.text != confirmContrasController.text){
-                      Get.snackbar('Error', 'Las contraseñas no coinciden', snackPosition: SnackPosition.BOTTOM);
-                      return;
-                    }
-                  }
-
-                  Usuario tempUsuario = Usuario(
-                    correo: correoController.text,
-                    contras: contrasController.text, 
-                    tipoUsuario: 1,
-                    nombres: nombreController.text,
-                    apellido: apellidoController.text,
-                    cedula: cedulaController.text,
-                    direccion: direccionController.text,
-                    genero: dropdownValue,
-                    telefono: telefonoController.text,
-                    estado: estado
-                  );
-                  if(esEditar){
-                    tempUsuario.id = widget.usuario!.id!;
-                    bool response = await usuarioController.actualizarUsuario(tempUsuario);
-                    if(response){
-                      Get.snackbar('Actualizado', 'Se actualizó correctamente', snackPosition: SnackPosition.BOTTOM);
-                    }else{
-                      Get.snackbar('Error', 'Ha ocurrido un error al actualizar la cuenta', snackPosition: SnackPosition.BOTTOM);
-                    }
-                  }else{
-                    Usuario? usuRes = await loginController.registraEmail(tempUsuario);
-                    if(usuRes!=null){
-                      Get.snackbar('Creado', 'Se creo correctamente', snackPosition: SnackPosition.BOTTOM);
-                    }else{
-                      Get.snackbar('Error', 'Ha ocurrido un error al crear la cuenta', snackPosition: SnackPosition.BOTTOM);
-                    }
-                  }
-                }, 
-                child: Text(esEditar ? 'Modificar' : 'Registrarse')
-              ),
-            ],
-          ),
-          const SizedBox(height: 20,)
-        ],
+            const SizedBox(
+              height: 20,
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/app/ui/pages/login.dart
+++ lib/app/ui/pages/login.dart
@@ -3,95 +3,161 @@ import 'package:get/get.dart';
 import '../../controllers/loginController.dart';
 import 'crear_cuenta.dart';
 
+class LoginPage extends StatefulWidget {
+  LoginPage({super.key});
 
-class LoginPage extends StatelessWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
 
+class _LoginPageState extends State<LoginPage> {
+  final _keyFrom = GlobalKey<FormState>();
   TextEditingController correoController = TextEditingController();
+
   TextEditingController contrasenaController = TextEditingController();
 
   LoginController loginController = Get.find();
-
-  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
+          child: Form(
+        key: _keyFrom,
         child: ListView(
           children: [
-            const SizedBox(height: 100,),
+            const SizedBox(
+              height: 100,
+            ),
             Image.asset(
               'assets/logo.png',
               width: 200,
               height: 200,
             ),
-            const SizedBox(height: 50,),
-            InputLogin(controller: correoController, texto: 'Correo', placeholder: 'Ingrese su correo',),
-            const SizedBox(height: 20,),
-            InputLogin(controller: contrasenaController, texto: 'Contraseña', esContrasena: true, placeholder: 'Ingrese la contraseña',),
-            const SizedBox(height: 20,),
+            const SizedBox(
+              height: 50,
+            ),
+            InputLogin(
+              controller: correoController,
+              texto: 'Correo',
+              placeholder: 'Ingrese su correo',
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            InputLogin(
+              controller: contrasenaController,
+              texto: 'Contraseña',
+              esContrasena: true,
+              placeholder: 'Ingrese la contraseña',
+            ),
+            const SizedBox(
+              height: 20,
+            ),
             Column(
               children: [
                 ElevatedButton(
-                  onPressed: (){
-                    if(correoController.text.isEmpty || contrasenaController.text.isEmpty){
-                      Get.snackbar('Error', 'Todos los campos son requridos', snackPosition: SnackPosition.BOTTOM);
-                      return;
-                    }
-
-                    loginController.ingresarEmail(correoController.text, contrasenaController.text);
-                  }, 
-                  child: const Text('Iniciar sesion')
+                    onPressed: () {
+                      if (_keyFrom.currentState!.validate()) {
+                        if (correoController.text.isEmpty ||
+                            contrasenaController.text.isEmpty) {
+                          Get.snackbar(
+                              'Error', 'Todos los campos son requridos',
+                              snackPosition: SnackPosition.BOTTOM);
+                          return;
+                        }
+                        loginController.ingresarEmail(
+                            correoController.text, contrasenaController.text);
+                      }
+                    },
+                    child: const Text('Iniciar sesion')),
+                const SizedBox(
+                  height: 10,
                 ),
-                const SizedBox(height: 10,),
                 ElevatedButton(
-                  onPressed: (){
+                  onPressed: () {
                     Get.to(CrearCuentaPage());
-                  }, 
+                  },
                   child: const Text('Registrarse'),
                 ),
               ],
             ),
           ],
         ),
-      ),
+      )),
     );
   }
 }
 
-
 class InputLogin extends StatelessWidget {
-  
   final TextEditingController? controller;
   final String texto;
+  final dynamic funcion;
   final String? placeholder;
   final bool? esContrasena;
   final TextInputType tipoTeclado;
   final bool editable;
 
-  const InputLogin({
-    Key? key,
-    this.controller,
-    required this.texto,
-    this.esContrasena,
-    this.placeholder,
-    this.tipoTeclado = TextInputType.text, this.editable = true
-  }) : super(key: key);
-
+  const InputLogin(
+      {Key? key,
+      this.controller,
+      this.funcion,
+      required this.texto,
+      this.esContrasena,
+      this.placeholder,
+      this.tipoTeclado = TextInputType.text,
+      this.editable = true})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal:20.0),
-      child: TextField(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextFormField(
+        validator: (value) {
+          if (value!.isEmpty) {
+            return "Campo Vacio";
+          } else {
+            if (texto == "Contraseña") {
+              if (value.length < 6 || value.length > 11) {
+                return "El rango es de 6 a 20";
+              }
+            }
+            if (texto == "Correo") {
+              if (value!.isEmpty == value.isEmail) {
+                return "Dominio Icorrecto";
+              }
+            }
+            if (texto == "Cedula") {
+              if (value.length <= 5 || value.length >= 11) {
+                return "El nombre es 5 a 11";
+              }
+            }
+            if (texto == 'Telefono') {
+              if (value.length != 10) {
+                return "Numero Telefono Incorrecto";
+              }
+            }
+            if (texto == 'Direccion') {
+              if (value.length <= 5 || value.length >= 40) {
+                return "Perro huputa esta la direccion mala";
+              }
+            }
+            if (texto == 'Nombres' || texto == 'Apellidos') {
+              if (value.length <= 3 || value.length >= 40) {
+                return "Rango es de 3 a 40";
+              }
+            }
+          }
+          return null;
+        },
+        onEditingComplete: funcion,
         controller: controller,
         obscureText: esContrasena ?? false,
         keyboardType: tipoTeclado,
         enabled: editable,
         decoration: InputDecoration(
-          labelStyle: const TextStyle(
-            color: Colors.blue
-          ),
+          labelStyle: const TextStyle(color: Colors.blue),
           labelText: texto,
           hintText: placeholder ?? '',
           border: OutlineInputBorder(
